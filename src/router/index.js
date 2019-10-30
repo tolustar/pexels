a6c02736ae957fc31cf7f759cea957e4aba1924d
Olaogun Toluwalase <tolustar29@gmail.com>
2019-10-30 09:43:55 +0100

    Updated PWA manifest icons Included Bootstrap library in index.html for better aesthetics

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
@@ -13,17 +14,22 @@ const routes = [
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: About
   }
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
+  scrollBehavior(to) {
+    if(to.query.page == null){
+      
+      return {
+        x: 0,
+        y: 0
+      }
+    }
+  },
   routes
 });
 
